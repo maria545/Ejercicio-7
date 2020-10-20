c84e0f56bbd028c7722cef97f7ba2c7f2bc1b42d
Maria <Maria@DESKTOP-G07TP2O>
2020-10-20 17:19:19 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio 7/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio_7
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton boton = (RadioButton)sender;
+            DINTTextBlock.FontSize = Convert.ToDouble(boton.Tag);
+            
+        }
+    }
+}
